fcd8807aff1906a8408122e5c1eff7aa197d4656
Phil Henrickson <phil.henrickson@gmail.com>
2021-12-30 20:51:00 -0600

    added 12/30/21

---
+++ predict_ratings/run_examine_models.R
@@ -0,0 +1,9 @@
+# run examine models
+library(tidyverse)
+library(foreach)
+
+rmarkdown::render(here::here("predict_ratings/examine_models.Rmd"), 
+                  output_file =  paste("report_",
+                                       Sys.Date(),
+                                       sep=""),
+                  output_dir = here::here("predict_ratings/predictions"))
