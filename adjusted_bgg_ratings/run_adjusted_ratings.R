39f3180cf9f28f1efe01f3a5fe4c22db2061ad11
Phil Henrickson <phil.henrickson@gmail.com>
2022-01-05 10:12:49 -0600

    script for daily run of adjusted bgg ratings

---
+++ adjusted_bgg_ratings/run_adjusted_ratings.R
@@ -0,0 +1,9 @@
+# run examine models
+library(tidyverse)
+library(foreach)
+
+# run, update active html file
+# also saves data from day of run
+rmarkdown::render(here::here("adjusted_bgg_ratings/notebook_adjust_ratings.Rmd"), 
+                  output_file =  "adjusted_bgg_ratings",
+                  output_dir = here::here("adjusted_bgg_ratings/"))
