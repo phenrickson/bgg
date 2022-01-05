d45280ab856990b5bed8ab912d7f2fe3eead9b7b
Phil Henrickson <phil.henrickson@gmail.com>
2022-01-05 11:59:54 -0600

    updated 1/5/22

--- adjusted_bgg_ratings/run_adjusted_ratings.R
+++ adjusted_bgg_ratings/run_adjusted_ratings.R
@@ -7,3 +7,5 @@ library(foreach)
 rmarkdown::render(here::here("adjusted_bgg_ratings/notebook_adjust_ratings.Rmd"), 
                   output_file =  "adjusted_bgg_ratings",
                   output_dir = here::here("adjusted_bgg_ratings/"))
+
+rm(list=ls())
--- predict_ratings/run_examine_models.R
+++ predict_ratings/run_examine_models.R
@@ -3,7 +3,10 @@ library(tidyverse)
 library(foreach)
 
 rmarkdown::render(here::here("predict_ratings/examine_models.Rmd"), 
-                  output_file =  paste("report_",
-                                       Sys.Date(),
-                                       sep=""),
+                  # output_file =  paste("report_",
+                  #                      Sys.Date(),
+                  #                      sep=""),
+                  output_file = "predicted_bgg_ratings",
                   output_dir = here::here("predict_ratings/predictions"))
+
+rm(list=ls())
